c2bbed6f8cf05600f62b5c4702ae210987bb4919
Cristian Gonzalez <albertsevilla1996@gmail.com>
2023-03-23 10:02:49 -0600

    feat: :sparkles: configure main file add config service to access to env vars configure cors configure helmet configure port

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
+import corsOptions from '../config/corsOptions';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  const whitelist = configService.get('server.allowed_sources');
+  app.use(helmet());
+  app.enableCors(corsOptions(whitelist));
+  await app.listen(configService.get('server.port'));
 }
 bootstrap();
