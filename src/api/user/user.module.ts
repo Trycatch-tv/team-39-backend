bc407c2e61debe364935659003a2dba7949adfa2
Cristian Gonzalez <albertsevilla1996@gmail.com>
2023-03-23 22:31:59 -0600

    feat: :sparkles: create user module

---
+++ src/api/user/user.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { UserController } from './infrastructure/controller/user.controller';
+import { MysqlRepositoryService } from './infrastructure/repository/mysql.repository.service';
+
+@Module({
+  controllers: [UserController],
+  providers: [MysqlRepositoryService],
+})
+export class UserModule {}
