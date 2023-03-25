e0fd463a5b1623e3461130321b422492be5686e5
Cristian Gonzalez <albertsevilla1996@gmail.com>
2023-03-24 21:18:56 -0600

    feat: :sparkles: configure user use case and add put method

--- src/api/user/infrastructure/controller/user.controller.ts
+++ src/api/user/infrastructure/controller/user.controller.ts
@@ -4,28 +4,39 @@ import {
   Get,
   Param,
   Post,
+  Put,
   UseInterceptors,
 } from '@nestjs/common';
 import { FormatResponseInterceptor } from '../../../../../src/interceptors/format-response.interceptor';
 import { MysqlRepositoryService } from '../repository/mysql.repository.service';
-import LoginDto from '../../application/login.dto';
+import { UserUseCase } from '../../application/user.use-case';
+import UserDto from '../../application/user.dto';
+import { IUserUseCase } from '../../domain/user.use-case';
 
 @UseInterceptors(FormatResponseInterceptor)
 @Controller('api/v1/user')
 export class UserController {
-  constructor(private readonly mysqlRepository: MysqlRepositoryService) {}
+  useCase: IUserUseCase;
+  constructor(private readonly mysqlRepository: MysqlRepositoryService) {
+    this.useCase = new UserUseCase(this.mysqlRepository);
+  }
 
-  @Get('/')
+  @Get()
   getUsers() {
-    return this.mysqlRepository.findAll();
+    return this.useCase.findUsers();
   }
-  @Get('/:id')
+  @Get(':id')
   getUser(@Param('id') id: string) {
-    return this.mysqlRepository.findById(id);
+    return this.useCase.findUser(id);
   }
 
-  @Post('/auth/login')
-  login(@Body() user: LoginDto) {
-    return user;
+  @Post()
+  createUser(@Body() user: UserDto) {
+    return this.useCase.createUser(user);
+  }
+
+  @Put(':id')
+  updateUser(@Param('id') id: string, @Body() user: UserDto) {
+    return this.useCase.updateUser(id, user);
   }
 }
