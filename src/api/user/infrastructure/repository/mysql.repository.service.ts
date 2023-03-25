6cf14f89fa300ad6f0bc9a57f98e61932feea615
Cristian Gonzalez <albertsevilla1996@gmail.com>
2023-03-24 21:49:38 -0600

    feat: :sparkles: add update method

--- src/api/user/infrastructure/repository/mysql.repository.service.ts
+++ src/api/user/infrastructure/repository/mysql.repository.service.ts
@@ -1,20 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import { UserEntity } from '../../domain/user.entity';
 import { UserRepository } from '../../domain/user.repository';
-import UserMockModel from '../model/user.model';
+import UserModel from '../model/user.model';
 
 @Injectable()
 export class MysqlRepositoryService implements UserRepository {
   findOne(): Promise<UserEntity> {
-    throw new Error('Method not implemented.');
+    return UserModel.findOne();
   }
   create(user: UserEntity): Promise<UserEntity> {
     throw new Error('Method not implemented.' + user);
   }
   findAll(): Promise<UserEntity[] | any> {
-    return UserMockModel.findAll();
+    return UserModel.findAll();
   }
   findById(id: string): Promise<UserEntity> {
-    return UserMockModel.findById(id);
+    return UserModel.findById(id);
+  }
+  update(id: string, user: UserEntity): Promise<UserEntity> {
+    return UserModel.update(id, user);
   }
 }
