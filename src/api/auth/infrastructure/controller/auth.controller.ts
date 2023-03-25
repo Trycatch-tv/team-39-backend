d849c7502e1e39df02f4a3ab818490719c31ba89
Cristian Gonzalez <albertsevilla1996@gmail.com>
2023-03-24 21:53:31 -0600

    feat: :sparkles: add auth controller

---
+++ src/api/auth/infrastructure/controller/auth.controller.ts
@@ -0,0 +1,9 @@
+import { Controller } from '@nestjs/common';
+// import { FormatResponseInterceptor } from 'src/interceptors/format-response.interceptor';
+
+// @UseInterceptors(FormatResponseInterceptor)
+@Controller('api/v1/auth')
+export class AuthController {
+  //   @Post()
+  //   login(@Body() user: LoginDto) {}
+}
