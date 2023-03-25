17c91a5ee24c537d86344fc852266855b2314f3d
Cristian Gonzalez <albertsevilla1996@gmail.com>
2023-03-24 21:13:59 -0600

    feat: :sparkles: add update method change register method to createuser

--- src/api/user/application/user.use-case.ts
+++ src/api/user/application/user.use-case.ts
@@ -1,11 +1,12 @@
 import { UserEntity } from '../domain/user.entity';
 import { UserRepository } from '../domain/user.repository';
 import UserValue from '../domain/user.value';
+import { IUserUseCase } from '../domain/user.use-case';
 
-export class UserUseCase {
+export class UserUseCase implements IUserUseCase {
   constructor(private readonly userRepository: UserRepository) {}
 
-  register = (user: UserEntity) => {
+  createUser = (user: UserEntity) => {
     const userCreated = this.userRepository.create(new UserValue(user));
     return userCreated;
   };
@@ -19,4 +20,8 @@ export class UserUseCase {
     const user = this.userRepository.findById(id);
     return user;
   };
+
+  updateUser(id: string, user: UserEntity) {
+    return this.userRepository.update(id, user);
+  }
 }
